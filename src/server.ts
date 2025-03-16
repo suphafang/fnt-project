7337503759d62a01891db046072cf54892591850
suphafang <suphachai.fang@gmail.com>
2025-03-16 22:49:10 +0700

    update: add @elysiajs/cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { Elysia } from 'elysia'
+import { cors } from '@elysiajs/cors'
 
 import env from './config'
 import database from './connections/mongoose'
@@ -6,7 +7,9 @@ import database from './connections/mongoose'
 async function main(): Promise<void> {
   try {
     await database.connect()
-    const app = new Elysia().listen(env.PORT)
+    const app = new Elysia()
+      .use(cors({ origin: 'http://localhost:3000' }))
+      .listen(env.PORT)
     console.log(`Server is running at ${app.server?.hostname}:${app.server?.port}`)
   } catch (error) {
     await database.disconnect()
